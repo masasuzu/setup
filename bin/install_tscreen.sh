13ca1b5b519186c13e635a0846765fd26f9f8dfc
masasuzu <masasuzu@lenny-trac.masasuzu-lab.net>
2010-09-07 22:47:30 +0900

    tscreenのインストールスクリプト

---
+++ bin/install_tscreen.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/zsh
+
+tmp_dir=${HOME}/tmp
+
+sudo aptitude update && sudo aptitude safe-upgrade
+sudo aptitude install make libreadline5-dev
+
+if [ ! -d ${tmp_dir} ]
+then
+    mkdir ${tmp_dir}
+fi
+
+cd ${tmp_dir}
+
+wget http://www.steve.org.uk/Software/tscreen/dist/tscreen-0.4.9.tar.gz
+tar zxvf tscreen-0.4.9.tar.gz
+cd tscreen-0.4.9
+
+./configure --enable-colors256 && make && sudo make install
+
