b6eee1e6ad0ae39f51725adb221cda34926c4360
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2011-09-09 22:54:38 +0900

    system pytnon rubyは入れない

--- linux/debian_setup/setup/init-install-lenny.sh
+++ linux/debian_setup/setup/init-install-lenny.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/zsh
 
 sudo aptitude update && sudo aptitude safe-upgrade && \
-sudo aptitude install gcc make libreadline5-dev python ruby git-core vim zsh sudo zlib1g-dev
+sudo aptitude install -y gcc make libreadline5-dev git-core vim zsh sudo zlib1g-dev
