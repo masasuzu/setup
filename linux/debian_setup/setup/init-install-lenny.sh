8b30a4d23bcfcd11c7a60753b425575a7a2db0ed
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2011-09-11 22:45:15 +0900

    build-essentialで入るはず

--- linux/debian_setup/setup/init-install-lenny.sh
+++ linux/debian_setup/setup/init-install-lenny.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/zsh
 
 sudo aptitude update && sudo aptitude safe-upgrade && \
-sudo aptitude install -y gcc make libreadline5-dev git-core vim zsh sudo zlib1g-dev
+sudo aptitude install -y build-essential libreadline5-dev git-core vim zsh sudo zlib1g-dev
